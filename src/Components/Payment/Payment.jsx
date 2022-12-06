2c02ed2e9baf7ae488d8ed530266765b35f44a70
hau165 <20521301@gm.uit.edu.vn>
2022-12-07 00:06:58 +0700

    update page payment

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -4,10 +4,11 @@ import {NavLink as Link} from "react-router-dom";
 // import { render } from "@testing-library/react";
 function Payment()
 {
-    const [isShown, setIsShown] = useState(true);
-    const handleClick = event => {
-        setIsShown(current => !current);
-      };
+    // const [isShown, setIsShown] = useState(true);
+    // const bank = event => {
+    //     setIsShown(current => !current);
+    //   };
+    const [visible, setVisible] = useState(false);
     return(
         <div className="main-container-payment">
             <div className="nav">
@@ -21,20 +22,20 @@ function Payment()
             <h2>Phương thức thanh toán</h2>
             <div className="form-payment-container">
                 <div className="form-payment">
-                    <input type="radio" name="payment" value="cod" id="payment_cod"/>Thanh toán khi nhận hàng (COD)
+                    <input type="radio" name="payment" value="cod" id="payment_cod" onClick={() => setVisible(false)}/>Thanh toán khi nhận hàng (COD)
                 </div>
 
                 <div className="form-payment">
-                    <input type="radio" name="payment" value="bank" id="payment_bank" onClick={handleClick}/>Chuyển khoản
+                    <input type="radio" name="payment" value="bank" id="payment_bank" onClick={() => setVisible(true)}/>Chuyển khoản
                 </div>
 
-                <div className="form-payment_bank" style={{display: isShown ? 'none' : 'block'}}>
+                {visible && <div className="form-payment_bank">
                     <p>Vietcombank chi nhánh HCM</p>
                     <p>Số tài khoản: 1016956832</p>
                     <p>Tên tài khoản: Nguyễn Quốc Kỳ</p>
                     <p>Nhận viên sẽ gọi điện xác nhận sau khi nhận được chuyển khoản</p>
                     <p>Cảm ơn bạn đã mua sắm tại TechIE Shop</p>
-                </div>
+                </div>}
                 <Link to="/"><button type="submit" name="submit" className="btn-Submit">Hoàn tất mua hàng</button></Link>
             </div>
         </div>
