4a89a74b4b38dd61cfdf0dd6f36596252e6e4c02
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-12-11 17:47:24 +0700

    update Header.jsx

--- src/Components/HeaderFolder/Header.jsx
+++ src/Components/HeaderFolder/Header.jsx
@@ -8,27 +8,12 @@ import Searchbar from "../Search/Searchbar";
 import { Log_out } from "../../actions";
 import { useNavigate } from "react-router-dom";
 
-import Register from "../Register/Register";
-import { Avatar } from "@mui/material";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../LoginFolder/Login";
 import { connect } from "react-redux";
-// https://png.pngtree.com/png-vector/20190114/ourlarge/pngtree-vector-avatar-icon-png-image_313572.jpg
 
 function Header(props) {
-  let logged_fullName = "";
-  let headerData = [];
-  headerData = JSON.parse(localStorage.getItem("da_dang_ky"));
-  headerData.map(item => {
-    logged_fullName = item.fullname;
-  })
   let navigate = useNavigate();
-  var linkCart = "/Login";
-  var loginButtonText = "Đăng nhập";
-  if (props.isLoggedin) {
-    linkCart = "/ShoppingCart";
-    loginButtonText = "Đăng xuất";
-  }
+  const [loginButtonText, setloginButtonText] = useState("Đăng nhập");
+
   function LoginclickHandler() {
     if (props.isLoggedin) {
       props.Log_out();
@@ -36,49 +21,6 @@ function Header(props) {
     } else {
       navigate("/Login");
     }
-  }
-  if (props.isLoggedin) {
-    return (
-      <div className="header">
-      <div className="header-left">
-        <Link to="/">
-          <img src={logo} alt="logo" />
-          <h2>TechieShop</h2>
-        </Link>
-      </div>
-
-      <div className="header-nav_menu">
-        <button>
-          <NavLink to="/MainPage">Trang chủ</NavLink>
-        </button>
-        <button>
-          <NavLink to="/Products">Sản phẩm</NavLink>
-        </button>
-        <button>
-          <NavLink to="/AboutUs">Giới thiệu</NavLink>
-        </button>
-      </div>
-
-      <Searchbar />
-
-      <div className="header-right">
-        <div className="header-right_item">
-          <Link to={linkCart}>
-            <img src={shoppingIcon} alt="cart" />
-            <span>{props.numberCart}</span>
-          </Link>
-          <Avatar 
-            className="avatar_login"
-            src="https://png.pngtree.com/png-vector/20190114/ourlarge/pngtree-vector-avatar-icon-png-image_313572.jpg"
-          />
-          <a href="#" className="logged_username" >{logged_fullName}</a>
-          <button onClick={LoginclickHandler} className="logIn-btn">
-            {loginButtonText}
-          </button>
-        </div>
-      </div>
-    </div>
-    )
   }
   return (
     <div className="header">
@@ -104,15 +46,27 @@ function Header(props) {
       <Searchbar />
 
       <div className="header-right">
-        <div className="header-right_item">
-          <Link to={linkCart}>
-            <img src={shoppingIcon} alt="cart" />
-            <span>{props.numberCart}</span>
-          </Link>
-          <button onClick={LoginclickHandler} className="logIn-btn">
-            {loginButtonText}
-          </button>
-        </div>
+        {props.isLoggedin ? (
+          <div className="header-right_item">
+            <Link to="/ShoppingCart">
+              <img src={shoppingIcon} alt="cart" />
+              <span>{props.numberCart}</span>
+            </Link>
+            <button onClick={() => LoginclickHandler()} className="logIn-btn">
+              Đăng xuất
+            </button>
+          </div>
+        ) : (
+          <div className="header-right_item">
+            <Link to="/Login">
+              <img src={shoppingIcon} alt="cart" />
+              <span>{props.numberCart}</span>
+            </Link>
+            <button onClick={() => LoginclickHandler()} className="logIn-btn">
+              Đăng nhập
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -130,4 +84,3 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
-
