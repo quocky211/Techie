cea915d09ec08676b1ae5c7e8525a497b41fd3bf
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-12-11 21:47:41 +0700

    added new Login.jsx and Register.jsx

--- src/Components/LoginFolder/Login.jsx
+++ src/Components/LoginFolder/Login.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { Log_in } from "../../actions";
 import { connect } from "react-redux";
 import { useState } from "react";
+import Register from "../Register/Register";
 
 function Login(props) {
   const [username, setUsername] = useState("");
@@ -35,11 +36,16 @@ function Login(props) {
     );
   };
   const handleClick = () => {
-    if (username !== "" && password !== "") {
-      props.Log_in();
-      navigate("/MainPage");
-      console.log(props.isLoggedin);
-    }
+    let currentData = [];
+    currentData = JSON.parse(localStorage.getItem("da_dang_ky"));
+    currentData.map(item => {
+      if (item.username == username && item.password == password) {
+        props.Log_in();
+        navigate("/MainPage");
+        console.log(props.isLoggedin);
+      }
+    })
+    
   };
   return (
     <div className="loginmain">
